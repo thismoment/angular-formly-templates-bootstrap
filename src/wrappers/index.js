dbf01e0408be4aa51fdab1b061b828dae1fd93e5
Spencer Christensen <spencer@thismoment.com>
2023-03-17 05:42:20 -0500

    build

--- src/wrappers/index.js
+++ src/wrappers/index.js
@@ -1,22 +1,20 @@
-export default ngModule => {
+export default (ngModule) => {
   ngModule.config(addWrappers);
-
-  require('dynamic-bind-html');
 
   function addWrappers(formlyConfigProvider) {
     formlyConfigProvider.setWrapper([
       {
-        name: 'bootstrapLabel',
-        template: require('./label.html'),
-        apiCheck: check => ({
+        name: "bootstrapLabel",
+        template: require("./label.html"),
+        apiCheck: (check) => ({
           templateOptions: {
             label: check.string.optional,
             required: check.bool.optional,
             labelSrOnly: check.bool.optional,
-          }
-        })
+          },
+        }),
       },
-      {name: 'bootstrapHasError', template: require('./has-error.html')}
+      { name: "bootstrapHasError", template: require("./has-error.html") },
     ]);
   }
 };
